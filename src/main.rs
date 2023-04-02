2d150ed31e2ca482a97a9ab8b9d637d37059ba60
tidub <57918941+tidub@users.noreply.github.com>
2023-04-02 16:17:08 +0100

    update dependencies, add style asset, cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 use std::io::Error;
 
 use cursive::event::Event;
-use cursive::view::Resizable;
+use cursive::view::{Resizable, Scrollable};
 use cursive::Cursive;
+
+use crate::args::Args;
+use crate::player::Player;
+use crate::player_view::PlayerView;
 
 mod args;
 mod audio_file;
 mod player;
 mod player_view;
-
-use crate::args::Args;
-use crate::player::Player;
-use crate::player_view::PlayerView;
 
 fn main() {
     let result = run();
@@ -24,15 +24,17 @@ fn main() {
 }
 
 fn run() -> Result<(), Error> {
-    let (mut player, size) = Player::new(Args::parse_args()?);
+    let (player, size) = Player::new(Args::parse_args()?);
     let mut cursive = cursive::default();
 
-    player.play_or_pause();
+    cursive
+        .load_toml(include_str!("assets/style.toml"))
+        .unwrap();
 
     cursive.add_layer(
         PlayerView::new(player, size)
             .full_width()
-            .fixed_height(size),
+            .fixed_height(size), // .scrollable()
     );
 
     cursive.set_on_pre_event(Event::Char('q'), |c: &mut Cursive| c.quit());
