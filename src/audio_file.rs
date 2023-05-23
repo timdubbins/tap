a1eafd139f004ca851ea51b10b5fc303f51e01ed
tidub <57918941+tidub@users.noreply.github.com>
2023-05-23 22:30:06 +0100

    error handling

--- src/audio_file.rs
+++ src/audio_file.rs
@@ -1,6 +1,7 @@
 use core::cmp::Ordering;
 use std::path::PathBuf;
 
+use anyhow::bail;
 use lofty::{Accessor, AudioFile as LoftyAudioFile, Probe, TaggedFileExt};
 
 #[derive(Clone, Debug, Eq, PartialEq, Ord)]
@@ -16,22 +17,28 @@ pub struct AudioFile {
 }
 
 impl AudioFile {
-    pub fn new(path: PathBuf) -> Self {
-        let tagged_file = Probe::open(&path)
-            .expect("ERROR: Bad path provided!")
-            .read()
-            .expect("ERROR: Failed to read file!");
+    pub fn new(path: PathBuf) -> Result<Self, anyhow::Error> {
+        let file = Probe::open(&path)
+            .expect("the path of `tagged_file` is provided by `Player::create_playlist()`");
+
+        let tagged_file = match file.read() {
+            Ok(f) => f,
+            Err(e) => bail!("Failed to read '{:?}'. Error: {}", path, e),
+        };
 
         let tag = match tagged_file.primary_tag() {
             Some(primary_tag) => primary_tag,
-            None => tagged_file.first_tag().expect("ERROR: No tags found!"),
+            None => match tagged_file.first_tag().ok_or(()) {
+                Ok(t) => t,
+                Err(_) => bail!("No tags found for '{:?}", path),
+            },
         };
 
         let properties = tagged_file.properties();
         let artist = tag.artist().as_deref().unwrap_or("None").trim().to_string();
         let duration = properties.duration().as_secs() as usize;
 
-        Self {
+        let audio_file = Self {
             album: tag.album().as_deref().unwrap_or("None").trim().to_string(),
             title: tag.title().as_deref().unwrap_or("None").trim().to_string(),
             year: tag.year(),
@@ -40,7 +47,9 @@ impl AudioFile {
             artist,
             path,
             duration,
-        }
+        };
+
+        Ok(audio_file)
     }
 }
 
