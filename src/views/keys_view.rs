bf5c5c67801ecce4c1b9b33ef482eada1f849566
tidub <57918941+tidub@users.noreply.github.com>
2023-08-21 11:14:19 +0100

    update keybindings

--- src/views/keys_view.rs
+++ src/views/keys_view.rs
@@ -22,38 +22,40 @@ impl KeysView {
                     .child(
                         Dialog::new().title("Global").content(
                             ListView::new()
-                                .child("fuzzy search", TextView::new("`Tab`"))
-                                .child("filtered search", TextView::new("`A...Z`"))
-                                .child("sorted search", TextView::new("`Ctrl` + `s`"))
-                                .child("previous selection", TextView::new("`-`"))
-                                .child("random selection", TextView::new("`=`")),
+                                .child("fuzzy search:", TextView::new("Tab"))
+                                .child("depth search:", TextView::new("F1...F4"))
+                                .child("filtered search:", TextView::new("A...Z"))
+                                .child("artist search:", TextView::new("Ctrl + s"))
+                                .child("album search:", TextView::new("Ctrl + a"))
+                                .child("load previous album:", TextView::new("-"))
+                                .child("load random album:", TextView::new("=")),
                         ),
                     )
                     .child(DummyView.fixed_height(1))
                     .child(
                         Dialog::new().title("Player").content(
                             ListView::new()
-                                .child("play", TextView::new("`h` or `←` or `Space`"))
-                                .child("next", TextView::new("`j` or `↓`"))
-                                .child("previous", TextView::new("`k` or `↑`"))
-                                .child("stop", TextView::new("`l` or `→` or `Enter`"))
-                                .child("go to first track", TextView::new("`gg`"))
-                                .child("go to last track", TextView::new("`Ctrl` + `g`"))
-                                .child("go to track number", TextView::new("`0...9` + `g`"))
-                                .child("mute", TextView::new("`m`"))
-                                .child("help", TextView::new("`?`"))
-                                .child("quit", TextView::new("`q`")),
+                                .child("play:", TextView::new("h or ← or Space"))
+                                .child("next:", TextView::new("j or ↓"))
+                                .child("previous:", TextView::new("k or ↑"))
+                                .child("stop:", TextView::new("l or → or Enter"))
+                                .child("go to first track:", TextView::new("gg"))
+                                .child("go to last track:", TextView::new("Ctrl + g"))
+                                .child("go to track number:", TextView::new("0...9 + g"))
+                                .child("mute:", TextView::new("m"))
+                                .child("help:", TextView::new("?"))
+                                .child("quit:", TextView::new("q")),
                         ),
                     )
                     .child(DummyView.fixed_height(1))
                     .child(
                         Dialog::new().title("Fuzzy").content(
                             ListView::new()
-                                .child("clear search", TextView::new("`Ctrl` + `u`"))
-                                .child("cancel search     ", TextView::new("`Esc`"))
-                                .child("page up", TextView::new("`Ctrl` + `h` or `PgUp`"))
-                                .child("page down", TextView::new("`Ctrl` + `l` or `PgDn`"))
-                                .child("random page", TextView::new("`Ctrl` + `z`")),
+                                .child("clear search:", TextView::new("Ctrl + u"))
+                                .child("cancel search:", TextView::new("Esc"))
+                                .child("page up:", TextView::new("Ctrl + h or PgUp"))
+                                .child("page down:", TextView::new("Ctrl + l or PgDn"))
+                                .child("random page:", TextView::new("Ctrl + z")),
                         ),
                     )
                     .child(DummyView.fixed_height(2))
