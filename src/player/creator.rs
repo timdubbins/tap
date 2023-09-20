52202f368912f00181f52dee4e586feb1cb53043
tidub <57918941+tidub@users.noreply.github.com>
2023-09-21 00:21:14 +0100

    add PlayerCreator

---
+++ src/player/creator.rs
@@ -0,0 +1,123 @@
+use std::path::PathBuf;
+
+use anyhow::bail;
+use cursive::{Cursive, XY};
+
+use crate::utils::{random, UserData};
+
+use super::{player::PlayerResult, Player, PlayerOpts};
+
+#[derive(PartialEq)]
+pub enum PlayerCreator {
+    FuzzyFinder,
+    PreviousAlbum,
+    PreviousTrack,
+    RandomAlbum,
+    RandomTrack,
+}
+
+impl PlayerCreator {
+    pub fn from(&self, path: Option<PathBuf>, siv: &mut Cursive) -> PlayerResult {
+        match self {
+            Self::FuzzyFinder => Self::fuzzy(path, siv),
+            Self::PreviousAlbum | Self::PreviousTrack => Self::previous(&self, siv),
+            Self::RandomAlbum | Self::RandomTrack => Self::random(&self, siv),
+        }
+    }
+
+    pub fn new(path: PathBuf) -> PlayerResult {
+        let track = (path, 0);
+        let opts = PlayerOpts::default();
+
+        Player::new(track, opts, false)
+    }
+
+    fn previous(&self, siv: &mut Cursive) -> PlayerResult {
+        let ((path, mut index), opts) = siv
+            .with_user_data(|(opts, _, queue): &mut UserData| {
+                let (path, index) = queue.front().expect("should always exist").to_owned();
+                let opts: PlayerOpts = (*opts).into();
+
+                if queue.len() != 1 {
+                    queue.swap(0, 1);
+                    ((Some(path), index), opts)
+                } else {
+                    ((None, index), opts)
+                }
+            })
+            .expect("should be set on init");
+
+        if Self::PreviousAlbum.eq(self) {
+            index = 0
+        }
+
+        let is_randomized = Self::PreviousTrack.eq(self);
+
+        match path {
+            Some(path) => Player::new((path, index), opts, is_randomized),
+            None => bail!("path not set"),
+        }
+    }
+
+    fn random(&self, siv: &mut Cursive) -> PlayerResult {
+        let ((path, mut index), opts) = siv
+            .with_user_data(|(opts, paths, queue): &mut UserData| {
+                let opts: PlayerOpts = (*opts).into();
+                let (path, index) = queue.back().expect("should always exist").to_owned();
+
+                if queue.len() == 1 {
+                    let front = queue.front().expect("should always exist").to_owned();
+                    queue.push_back(front);
+                } else {
+                    queue.pop_front();
+                }
+
+                let next_random = match Player::randomized(&paths) {
+                    Some(track) => track,
+                    None => {
+                        let path = path.to_owned();
+                        let index = random(
+                            0..Player::playlist(&path)
+                                .expect("should always exist")
+                                .0
+                                .len(),
+                        );
+                        (path, index)
+                    }
+                };
+
+                queue.push_back(next_random);
+
+                ((path, index), opts)
+            })
+            .expect("should be set on init");
+
+        if Self::RandomAlbum.eq(self) {
+            index = 0;
+        }
+
+        Player::new((path, index), opts, Self::RandomTrack.eq(self))
+    }
+
+    fn fuzzy(path: Option<PathBuf>, siv: &mut Cursive) -> PlayerResult {
+        let track = (path.expect("path should be provided by fuzzy-finder"), 0);
+
+        let opts = siv
+            .with_user_data(|(opts, _, queue): &mut UserData| {
+                let opts: PlayerOpts = (*opts).into();
+
+                if queue.len() == 1 {
+                    queue.push_front(track.clone());
+                    queue.push_front(track.clone());
+                } else {
+                    queue.pop_front();
+                    queue.insert(1, track.clone());
+                }
+
+                opts
+            })
+            .expect("should be set on init");
+
+        Player::new(track, opts, false)
+    }
+}
--- src/player/mod.rs
+++ src/player/mod.rs
@@ -1,10 +1,12 @@
 pub mod audio_file;
+pub mod creator;
 pub mod opts;
 pub mod player;
 pub mod status;
 
 pub use self::{
     audio_file::{is_valid, AudioFile},
+    creator::PlayerCreator,
     opts::PlayerOpts,
     player::Player,
     status::{BytesToStatus, PlayerStatus, StatusToBytes},
