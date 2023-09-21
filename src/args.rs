af530a2287ff956b2f749255491caa6bee007753
tidub <57918941+tidub@users.noreply.github.com>
2023-09-21 17:29:27 +0100

    bump to v0.4.5

--- src/args.rs
+++ src/args.rs
@@ -18,7 +18,7 @@ pub enum Opts {
 #[command(
     author = "Tim Dubbins",
     about = "An audio player for the terminal with fuzzy-finder",
-    version = "0.4.4"
+    version = "0.4.5"
 )]
 #[clap(group = ArgGroup::new("exclude_multiples").multiple(false))]
 #[clap(group = ArgGroup::new("conflicts_path").conflicts_with("path"))]
