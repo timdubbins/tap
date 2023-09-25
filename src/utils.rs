8689cccaa782343ec9750a8d6182fbe52c6ceb5a
tidub <57918941+tidub@users.noreply.github.com>
2023-09-25 16:19:41 +0100

    remove has_child functions

--- src/utils.rs
+++ src/utils.rs
@@ -113,46 +113,6 @@ impl TimerBool {
     }
 }
 
-pub fn has_child(path: &PathBuf) -> bool {
-    let iter = match path.read_dir() {
-        Ok(r) => r,
-        Err(_) => return false,
-    };
-    for entry in iter {
-        if let Ok(entry) = entry {
-            if entry.path().is_dir() {
-                return true;
-            }
-        }
-    }
-    false
-}
-
-// Returns true if the path has at least two children.
-pub fn has_child_dirs(path: &PathBuf) -> bool {
-    let mut has_child_dir = false;
-
-    let iter = match path.read_dir() {
-        Ok(r) => r,
-        Err(_) => return false,
-    };
-
-    for entry in iter {
-        if let Ok(entry) = entry {
-            if entry.path().is_dir() {
-                if has_child_dir {
-                    // The second child is found.
-                    return true;
-                } else {
-                    // The first child is found.
-                    has_child_dir = true;
-                }
-            }
-        }
-    }
-    false
-}
-
 // Maps the array to a single value, i.e. `[0, 1, 2]` -> `12`.
 pub fn concatenate(arr: &Vec<usize>) -> usize {
     arr.iter().fold(0, |acc, x| acc * 10 + x)
